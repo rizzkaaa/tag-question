c0f8f025f4ae3360ef9b8e1b96fe0190826ad7b6
rizka <schonsteemadchen@gmail.com>
2025-12-04 22:27:56 +0700

    update how to make

--- app/dashboard/how-to-make-tag-question/page.jsx
+++ app/dashboard/how-to-make-tag-question/page.jsx
@@ -28,7 +28,7 @@ export default function HowToMakePage() {
         </div>
       </div>
       <div className={styles.content}>
-        <div className={styles.borderBox}>
+        <div className={`${styles.borderBox} ${styles.wrapStart}`}>
           <div className={styles.text}>
             <p className={tenali_Ramakrishna.className}>
               A tag question is made by adding a short question at the end of a
@@ -36,16 +36,112 @@ export default function HowToMakePage() {
             </p>
           </div>
         </div>
-        <div className="wrapStep1">
+        <div className={styles.wrapStep1}>
           <div className={styles.borderBox}>
             <div className={styles.text}>
               <p className={tenali_Ramakrishna.className}>
-                To form a question tag, we use an:
+                To form a question tag, we use:
               </p>
             </div>
           </div>
           <div className={styles.line}></div>
+
           <div className={styles.branch}></div>
+          <div className={styles.wrapBranch}>
+            <div className={styles.wrap}>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    An auxiliary verb
+                  </p>
+                </div>
+              </div>
+              <div className={styles.line}></div>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    is, are, do, does, have, will, can, etc.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.wrap}>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    A subject pronoun
+                  </p>
+                </div>
+              </div>
+              <div className={styles.line}></div>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    I, you, they, we, he, she, it
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.wrapStep2}>
+          <div className={styles.wrap}>
+            <div className={styles.branch}>
+              <div className={styles.line}></div>
+            </div>
+            <div className={styles.wrapContent}>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    If the main sentence is positive
+                  </p>
+                </div>
+              </div>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    The question tag must be negative
+                  </p>
+                </div>
+              </div>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    Example: You are a student, aren’t you?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.wrap}>
+            <div className={styles.branch}>
+              <div className={styles.line}></div>
+            </div>
+            <div className={styles.wrapContent}>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    If the main sentence is negative
+                  </p>
+                </div>
+              </div>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    The question tag must be positive
+                  </p>
+                </div>
+              </div>
+              <div className={styles.borderBox}>
+                <div className={styles.text}>
+                  <p className={tenali_Ramakrishna.className}>
+                    Example: She isn’t here, is she?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.action}>
