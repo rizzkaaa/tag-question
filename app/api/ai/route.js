6b2550b6689044e9834dd20920a38b33f5b25bd5
yopaaa <pusi.ng027@gmail.com>
2025-12-06 17:41:16 +0700

    udapet

--- app/api/ai/route.js
+++ app/api/ai/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const GEMINI_API_KEY = process.env.GEMINI_API;
 const GEMINI_ENDPOINT = process.env.GEMINI_ENDPOINT;
 
 export async function POST(req) {
