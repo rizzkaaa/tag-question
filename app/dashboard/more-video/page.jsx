4f58817a5ee09a40ddce8e26b7f4abef271fa882
rizzkaaa <joremifasollasido@gmail.com>
2025-12-13 20:48:10 +0700

    update video

--- app/dashboard/more-video/page.jsx
+++ app/dashboard/more-video/page.jsx
@@ -4,6 +4,15 @@ import { luckiest_Guy, tenali_Ramakrishna } from "../../../public/font";
 import styles from "./page.module.css";
 
 export default function MoreVideo() {
+  const videos = [
+    "https://www.youtube.com/embed/xGq0BN7JkLU",
+    "https://www.youtube.com/embed/uYJaBYBWYYs",
+    "https://www.youtube.com/embed/BqwOgQN7DP4",
+    "https://www.youtube.com/embed/JuAnvVADyLc",
+    "https://www.youtube.com/embed/7thLgNgnVbY",
+    "https://www.youtube.com/embed/y_UxwCSQGLc"
+  ];
+
   return (
     <div className="container">
       <div className={styles.borderBox}>
@@ -14,17 +23,19 @@ export default function MoreVideo() {
         </div>
       </div>
       <div>
-        <div className={styles.wrap}>
-          <iframe
-            width="445"
-            height="250"
-            src="https://www.youtube.com/embed/xGq0BN7JkLU?si=4whG3bSuiMgpomMX"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-          ></iframe>
-        </div>
+        {videos.map((video, i) => (
+          <div className={styles.wrap} key={i}>
+            <iframe
+              width="445"
+              height="250"
+              src={video}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
       </div>
     </div>
   );
